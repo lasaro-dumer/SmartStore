eb5ea92f03a1dd68bd9029063c8e67acc3efbc70
Lásaro <lasaro.dumer@gmail.com>
2018-05-19 18:59:30 -0300

    User login updates

--- SmartStore.Data/Models/CredentialModel.cs
+++ SmartStore.Data/Models/CredentialModel.cs
@@ -5,6 +5,7 @@ namespace SmartStore.Data.Models
     public class CredentialModel
     {
         [Required]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         [Required]
         public string Password { get; set; }
--- SmartStore.Data/Models/UserDetails.cs
+++ SmartStore.Data/Models/UserDetails.cs
@@ -1,11 +1,17 @@
-﻿namespace SmartStore.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartStore.Data.Models
 {
     public class UserDetails
     {
         public string Email { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
+        [Display(Name = "Email confirmed")]
         public bool EmailConfirmed { get; set; }
     }
 }
